bf3985ce879c8b5c60dde5d91307b2f9abd10594
atsh-omlet <atshogt@gmail.com>
2025-10-05 15:47:20 +0900

    fixed critical issues in previous commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.cs360.eventtrackeratsushi"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.cs360.eventtrackingappatsushi"
