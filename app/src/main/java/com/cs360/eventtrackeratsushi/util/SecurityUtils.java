d06f4d748d0409a98b9eb7045e19204cb98b99bb
atsh-omlet <atshogt@gmail.com>
2025-10-10 18:08:24 +0900

    minor code cleanup

--- app/src/main/java/com/cs360/eventtrackeratsushi/util/SecurityUtils.java
+++ app/src/main/java/com/cs360/eventtrackeratsushi/util/SecurityUtils.java
@@ -1,26 +1,10 @@
 package com.cs360.eventtrackeratsushi.util;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
 import org.mindrot.jbcrypt.BCrypt;
 
 public class SecurityUtils {
-    /*
-    public static String hashPassword(String password) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hash = md.digest(password.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (byte b : hash) {
-                sb.append(String.format("%02x", b));
-            }
 
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("SHA-256 not supported",e);
-        }
-    }
-    */
     public static String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
